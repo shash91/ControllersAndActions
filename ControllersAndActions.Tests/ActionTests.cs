dd6c18408c332a8d25701c957ac16501bb72c0b9
shash91 <shashika@davton.com>
2016-05-06 11:26:17 +0530

    Rendering a View
    Returning HTML by rendering a View  and Unit test for that

---
+++ ControllersAndActions.Tests/ActionTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ControllersAndActions.Controllers;
+using System.Web.Mvc;
+
+namespace ControllersAndActions.Tests
+{
+    [TestClass]
+    public class ActionTests
+    {
+        [TestMethod]
+        public void ViewSelectionTest()
+        {
+            ExampleController target = new ExampleController();
+
+            ViewResult result = target.Index();
+
+            Assert.AreEqual("Homepage", result.ViewName);
+
+
+
+        }
+    }
+}
---
+++ ControllersAndActions/Controllers/ExampleController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ControllersAndActions.Controllers
+{
+    public class ExampleController : Controller
+    {
+        // GET: Example
+        public ViewResult Index()
+        {
+            return View("Homepage");
+        }
+    }
+}
